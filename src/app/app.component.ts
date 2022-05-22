9142cac280983b839fc2c4f3943c5de75135b84e
VeronikaUshakova <72931609+VeronikaUshakova@users.noreply.github.com>
2022-05-22 15:00:20 +0200

    Added project

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+export interface Todo{
+  id: number,
+  title: string,
+  completed: boolean,
+  date?: any
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  appTitle = "Angular app to-do application";
+}
